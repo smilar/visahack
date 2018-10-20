f3f80e682e2575349eee588f0991330dd4dc806c
Smilar <akudssi@gmail.com>
2018-10-20 13:19:57 -0700

    Initial push

---
+++ predict/src/test/java/com/chase/money/predict/PredictApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chase.money.predict;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PredictApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
