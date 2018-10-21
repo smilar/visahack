d168dbb22c3498bd7b1c290481615301cbfa33e3
Unknown <nick@nickstumpos.com>
2018-10-20 21:46:45 -0500

    grr local chacnges broke amazon

--- envelopes/src/main/java/com/chase/money/envelopes/data/LocalConfig.java
+++ envelopes/src/main/java/com/chase/money/envelopes/data/LocalConfig.java
@@ -1,27 +1,27 @@
 package com.chase.money.envelopes.data; 
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.h2.tools.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+// import org.h2.tools.Server;
 
-import java.sql.SQLException;
+// import java.sql.SQLException;
 
 
-@Configuration
-@EnableJpaRepositories
-@Profile("local")
+// @Configuration
+// @EnableJpaRepositories
+// @Profile("local")
 public class LocalConfig {
 	
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public Server h2Server() {
-		Server h2Server;
-		try {
-			h2Server = Server.createTcpServer();
-		} catch (SQLException e) {
-			throw new RuntimeException("Failed to start H2 server: ", e);
-		}
-		return h2Server;
-	}	
+	// @Bean(initMethod = "start", destroyMethod = "stop")
+	// public Server h2Server() {
+	// 	Server h2Server;
+	// 	try {
+	// 		h2Server = Server.createTcpServer();
+	// 	} catch (SQLException e) {
+	// 		throw new RuntimeException("Failed to start H2 server: ", e);
+	// 	}
+	// 	return h2Server;
+	// }	
 }
